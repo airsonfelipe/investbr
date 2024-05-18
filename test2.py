d76946581e8bf415c6e266cac4155404e172d40b
airsonfelipe <83257376+airsonfelipe@users.noreply.github.com>
2024-05-18 11:59:22 -0400

    add tickers
    Adicionei tickers.

--- test2.py
+++ test2.py
@@ -1,34 +1,10 @@
-import yfinance as yf
-from datetime import datetime
-def alta_dia():
-    with open("tickers.txt", "r") as file:
-        brazilian_tickers = [ticker.strip() for ticker in file.readlines()]
-
-    highest_gain = None  # Variável para armazenar a maior alta do dia
-    highest_gain_ticker = None  # Variável para armazenar o ticker com a maior alta do dia
-
-    # Define o horário de abertura do mercado (por exemplo, 10:00 da manhã)
-    market_open_time = datetime.now().replace(hour=10, minute=0, second=0, microsecond=0)
-
-    for ticker in brazilian_tickers:
-        try:
-            # Baixa os dados da ação
-            stock_data = yf.download(ticker, start=market_open_time)
-            open_price = stock_data["Open"].iloc[0]
-            close_price = stock_data["Close"].iloc[-1]
-            daily_gain = (close_price - open_price) / open_price * 100
-            if open_price == 0 or close_price == 0:
-                print(f"Preço de abertura ou de fechamento igual a zero para o ticker {ticker}. Pulando...")
-                continue
-        except Exception as e:
-            print(f"Erro ao baixar dados para o ticker {ticker}: {e}")
-            continue  # Pula para o próximo ticker
-
-    if highest_gain is None or daily_gain > highest_gain:
-        highest_gain = daily_gain
-        highest_gain_ticker = ticker
-
-    print(close_price, highest_gain_ticker, highest_gain)
-    return close_price
-
-alta_dia()
+def subroutine(n):
+    n = 10
+    while n != 1:
+        print(n, )
+        if n % 2 == 0:  # n is even
+            n = n // 2
+        else:  # n is odd
+            n = n * 3 + 1
+        
+subroutine(10)
